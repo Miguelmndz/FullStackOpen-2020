f2827233b7771e9731778de5ebfc57c174a4616b
miguel <michaelmndz1141@gmail.com>
2020-12-27 10:17:03 -0700

    1.14*: anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -9,15 +9,30 @@ const Button = ({handleClick, text}) => {
   )
 }
 
+
 const App = ({anecdotes}) => {
+  const emptyArray = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0)
+
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(emptyArray)
+  
+  
 
   const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const voteClick = () => {
+    const pointsCopy = [...points]
+    pointsCopy[selected] += 1
 
+    setPoints(pointsCopy)
+  }
   return (
     <div>
       {anecdotes[selected]}
       <Button handleClick={handleClick} text="next anecdotes"/>
+      <Button text="vote" handleClick={voteClick}/>
+      {points[selected]}
+  
+    
     </div>
   )
 }
@@ -31,7 +46,22 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+
+
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
+
+/* 
+what we first did is create a state of an empty array of zeros
+then need to create a handle to update the vote when the user clicks the button
+since we want to update the state we create a copy of that state
+
+
+1 
+
+
+
+
+*/
