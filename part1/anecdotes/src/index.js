418de5553d901502a9443745d1ec7eafb7ee7d7b
miguel <michaelmndz1141@gmail.com>
2020-12-28 11:51:20 -0700

    1.14*: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,36 +3,43 @@ import ReactDOM from 'react-dom'
 
 const Button = ({handleClick, text}) => {
   return(
-    <div>
     <button onClick={handleClick}>{text}</button>
-    </div>
   )
 }
-
 
 const App = ({anecdotes}) => {
   const emptyArray = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0)
 
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(emptyArray)
-  
-  
+  const [vote, setVoted] = useState(0)
 
   const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
-  const voteClick = () => {
+  const handleVote = () => {
+    
     const pointsCopy = [...points]
     pointsCopy[selected] += 1
 
     setPoints(pointsCopy)
+     // If the array element is greater at the random selected value than the mostVoted value
+     // then pass that into the modifying state fuction for mostVoted
+     // this will allow you keep that index postion to display the most voted anecdote 
+    if(pointsCopy[selected] > pointsCopy[vote]) 
+      setVoted(selected)
+
   }
   return (
     <div>
-      {anecdotes[selected]}
+      <h2>Anecdote of the day</h2>
+
+      <p>{anecdotes[selected]}. <br/>Has {points[selected]} votes.</p>
       <Button handleClick={handleClick} text="next anecdotes"/>
-      <Button text="vote" handleClick={voteClick}/>
-      {points[selected]}
-  
-    
+      <Button handleClick={handleVote} text="vote"/>
+      
+      
+      <h2>Anecdote with most votes</h2>
+      {anecdotes[vote]}
+      
     </div>
   )
 }
@@ -59,8 +66,12 @@ then need to create a handle to update the vote when the user clicks the button
 since we want to update the state we create a copy of that state
 
 
-1 
+Get Most Votes
 
+Need to check which qoute has the highest point count
+Need to get that vote to display 
+
+i have the max but dont have the index postion to display
 
 
 
